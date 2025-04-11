0e0001e54155ae97cf5978aff74327fbc6b18654
Aku <ap@Aku-MacBook-Air.local>
2025-04-11 19:32:27 +0300

    5.18

--- playwright/tests/blog_app.spec.js
+++ playwright/tests/blog_app.spec.js
@@ -10,7 +10,6 @@ describe('Blog app', () => {
         password: 'salainen'
       }
     })
-
     await page.goto('http://localhost:5173')
   })
 
@@ -19,5 +18,23 @@ describe('Blog app', () => {
     const locator2 = page.getByLabel(/password/i)
     await expect(locator1).toBeVisible()
     await expect(locator2).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByRole('textbox').first().fill('mluukkai')
+      await page.getByRole('textbox').last().fill('salainen')
+      await page.getByRole('button', { name: 'login' }).click()
+      await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByRole('textbox').first().fill('testusername')
+      await page.getByRole('textbox').last().fill('testpassword')
+      const locator1 = page.getByLabel(/username/i)
+      const locator2 = page.getByLabel(/password/i)
+      await expect(locator1).toBeVisible()
+      await expect(locator2).toBeVisible()
+    })
   })
 })
